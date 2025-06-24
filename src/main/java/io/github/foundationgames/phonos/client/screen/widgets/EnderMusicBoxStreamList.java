6eb798a4613e9319b4c6304225d98db66f6f9ae1
techno-sam <77073745+techno-sam@users.noreply.github.com>
2025-06-24 14:42:14 -0700

    fix: merge cleanup

--- src/main/java/io/github/foundationgames/phonos/client/screen/widgets/EnderMusicBoxStreamList.java
+++ src/main/java/io/github/foundationgames/phonos/client/screen/widgets/EnderMusicBoxStreamList.java
@@ -23,12 +23,7 @@ public class EnderMusicBoxStreamList extends AlwaysSelectedEntryListWidget<Ender
         return 320;
     }
 
-    @Override
-    protected int getScrollbarPositionX() { // fixme merge
-        return super.getScrollbarPositionX() + (getRowWidth() - super.getRowWidth())/2;
-    }
-
-    public static class Entry extends AlwaysSelectedEntryListWidget.Entry<io.github.foundationgames.phonos.client.screen.widgets.EnderMusicBoxStreamList.Entry> {
+    public static class Entry extends AlwaysSelectedEntryListWidget.Entry<Entry> {
         private final EnderMusicBoxBlockEntity entity;
         public final long id;
         private final String name;
